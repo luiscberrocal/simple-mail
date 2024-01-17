75355269abf147749e6e2589304912aca0ca349c
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-17 06:27:09 -0500

    Added fields to models.

--- email/models.py
+++ email/models.py
@@ -12,10 +12,10 @@ class SenderConfig(BaseModel):
 
 
 class EmailMessage(BaseModel):
-    recipients: List[str]
-    subject: str
-    content: str
-    sender_config: SenderConfig
+    recipients: List[str] = Field(description="Recipients")
+    subject: str = Field(description="Subject")
+    content: str = Field(description="Content")
+    sender_config: SenderConfig = Field(description="Sender configuration")
     attachments: Optional[List[Path] | None]
     format: EmailFormat = Field(default=EmailFormat.TEXT)
 
